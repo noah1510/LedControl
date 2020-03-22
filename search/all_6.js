491da2359fcba30226856e7851841f2a416fea14
noah1510 <noasakurajin@web.de>
2020-03-22 11:29:16 +0000

    Update gh-pages to output generated at 35e1697

--- search/all_6.js
+++ search/all_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['makecolumns',['makeColumns',['../class_led_controller.html#a6cde37ad176801ceafa357e20711432d',1,'LedController']]],
   ['max_5fsegments',['MAX_SEGMENTS',['../_led_controller_8hpp.html#a5155355bbabd39029d45b74529beba04',1,'LedController.hpp']]],
-  ['moveleft',['moveLeft',['../class_led_controller.html#afc6d671d091db03eeadbeee6b14fc539',1,'LedController']]]
+  ['moveleft',['moveLeft',['../class_led_controller.html#afc6d671d091db03eeadbeee6b14fc539',1,'LedController']]],
+  ['moveright',['moveRight',['../class_led_controller.html#aca0255f07b0349aed07f770dc3d18be9',1,'LedController']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['makecolumns',['makeColumns',['../class_led_controller.html#a6cde37ad176801ceafa357e20711432d',1,'LedController']]],
-  ['moveleft',['moveLeft',['../class_led_controller.html#afc6d671d091db03eeadbeee6b14fc539',1,'LedController']]]
+  ['moveleft',['moveLeft',['../class_led_controller.html#afc6d671d091db03eeadbeee6b14fc539',1,'LedController']]],
+  ['moveright',['moveRight',['../class_led_controller.html#aca0255f07b0349aed07f770dc3d18be9',1,'LedController']]]
 ];
