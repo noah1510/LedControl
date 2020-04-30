ca751587e2ace1fa6c8dd568f0a455681502ee24
noah1510 <noasakurajin@web.de>
2020-04-30 07:25:35 +0000

    Update gh-pages to output generated at 132b31b

--- search/all_5.js
+++ search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['getrow',['getRow',['../d9/def/class_led_controller.html#a3ecd47a8ae3ea19e61b6d9599a977065',1,'LedController']]],
   ['getsegmentcount',['getSegmentCount',['../d9/def/class_led_controller.html#a541961c0ca85cf4b132de50153a1df3a',1,'LedController']]]
 ];
